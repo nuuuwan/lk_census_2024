34912c5f75d904fddbda0bbea8d5882730749950
nuuuwan <nuuuwan@gmail.com>
2025-11-12 16:19:04 +0530

    Refactor population data constants in DataTableLoaderMixin; restore and structure A4, A5, A6 entries for clarity and maintainability

--- src/lk_census/data_table/DataTableLoaderMixin.py
+++ src/lk_census/data_table/DataTableLoaderMixin.py
@@ -1,59 +1,65 @@
 class DataTableLoaderMixin:
+    POPULATION_A4 = (
+        "Basic-Population-Information"
+        + "-by-Districts-and-Divisional-Secretary-Divisions",
+        "A4. Migrant population"
+        + " by reason for migrating according to districts",
+        (110, 111),
+        [],
+    )
+    POPULATION_A5 = (
+        "Basic-Population-Information"
+        + "-by-Districts-and-Divisional-Secretary-Divisions",
+        "A5. Population by sex, age and district according to DSD",
+        (112, 136),
+        [
+            "sex-male",
+            "sex-female",
+            "age-under-15",
+            "age-15-to-59",
+            "age-60-to-64",
+            "age-65-and-over",
+        ],
+    )
+    POPULATION_A6 = (
+        "Basic-Population-Information"
+        + "-by-Districts-and-Divisional-Secretary-Divisions",
+        "A6. Population by ethnicity and district according to DSD",
+        (137, 161),
+        [
+            "sinhalese",
+            "sri_lanka_tamil",
+            "indian_tamil_or_malaiyaga_thamilar",
+            "sri_lanka_moor_or_muslim",
+            "burgher",
+            "malay",
+            "sri_lanka_chetty",
+            "bharatha",
+            "veddhas",
+            "other",
+        ],
+    )
+    POPULATION_A7 = (
+        "Basic-Population-Information"
+        + "-by-Districts-and-Divisional-Secretary-Divisions",
+        "A7. Population by religion and district according to DSD",
+        (162, 185),
+        [
+            "buddhist",
+            "hindu",
+            "islam",
+            "roman_catholic",
+            "other_christian",
+            "other",
+        ],
+    )
+
     @classmethod
     def list_all(cls):
-        return [
-            # cls(
-            #     "Basic-Population-Information"
-            #     + "-by-Districts-and-Divisional-Secretary-Divisions",
-            #     "A4. Migrant population"
-            #     + " by reason for migrating according to districts",
-            #     (110, 111),
-            #     [],
-            # ),
-            # cls(
-            #     "Basic-Population-Information"
-            #     + "-by-Districts-and-Divisional-Secretary-Divisions",
-            #     "A5. Population by sex, age and district according to DSD",
-            #     (112, 136),
-            #     [
-            #         "sex-male",
-            #         "sex-female",
-            #         "age-under-15",
-            #         "age-15-to-59",
-            #         "age-60-to-64",
-            #         "age-65-and-over",
-            #     ],
-            # ),
-            # cls(
-            #     "Basic-Population-Information"
-            #     + "-by-Districts-and-Divisional-Secretary-Divisions",
-            #     "A6. Population by ethnicity and district according to DSD",
-            #     (137, 161),
-            #     [
-            #         "sinhalese",
-            #         "sri_lanka_tamil",
-            #         "indian_tamil_or_malaiyaga_thamilar",
-            #         "sri_lanka_moor_or_muslim",
-            #         "burgher",
-            #         "malay",
-            #         "sri_lanka_chetty",
-            #         "bharatha",
-            #         "veddhas",
-            #         "other",
-            #     ],
-            # ),
-            cls(
-                "Basic-Population-Information"
-                + "-by-Districts-and-Divisional-Secretary-Divisions",
-                "A7. Population by religion and district according to DSD",
-                (162, 185),
-                [
-                    "buddhist",
-                    "hindu",
-                    "islam",
-                    "roman_catholic",
-                    "other_christian",
-                    "other",
-                ],
-            ),
-        ]
+        for t in [
+            # cls.POPULATION_A4,
+            # cls.POPULATION_A5,
+            # cls.POPULATION_A6,
+            cls.POPULATION_A7,
+        ]:
+            yield cls(*t)
