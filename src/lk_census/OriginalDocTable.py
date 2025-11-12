89b6b47665486114c3b4095f9cf599839174cd55
nuuuwan <nuuuwan@gmail.com>
2025-11-12 12:19:38 +0530

    refactor OriginalDocTable to enhance data extraction and update JSON file naming

--- src/lk_census/OriginalDocTable.py
+++ src/lk_census/OriginalDocTable.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 import camelot
 from pypdf import PdfReader, PdfWriter
-from utils import Log, PDFFile
+from utils import JSONFile, Log, PDFFile
 
 log = Log("OriginalDocTable")
 
@@ -99,7 +99,9 @@ class OriginalDocTable:
                 plot_path = os.path.join(
                     self.dir_table, f"{table_id}.camelot.plot.png"
                 )
-                camelot.plot(table, kind="contour").savefig(plot_path)
+                camelot.plot(table, kind="contour").savefig(
+                    plot_path, dpi=300
+                )
                 log.debug(f"Saved plot to {plot_path}")
 
                 # Save table as JSON
@@ -107,31 +109,84 @@ class OriginalDocTable:
                 from utils import JSONFile
 
                 json_file = JSONFile(
-                    os.path.join(self.dir_table, f"{table_id}.json")
+                    os.path.join(self.dir_table, f"{table_id}.subtable.json")
                 )
                 json_file.write(table_data)
                 log.debug(f"Wrote {len(table_data)} rows to {json_file}")
 
+    def get_fields(self):
+        lattice_subtable_1 = JSONFile(
+            os.path.join(self.dir_table, "lattice-1.subtable.json")
+        )
+        field_groups = {}
+        subtable = lattice_subtable_1.read()
+        log.debug(f"{subtable=}")
+        for cells in subtable:
+            cur_cell = None
+            for i_cell, cell in enumerate(cells.values()):
+                cell = cell.replace("‐", "-")
+                cell_en_only = re.sub(r"[^A-Za-z0-9\s\-]", "", cell)
+                cell_en_only = re.sub(r"\s+", " ", cell_en_only).strip()
+                if i_cell not in field_groups:
+                    field_groups[i_cell] = []
+
+                if cell_en_only:
+                    cur_cell = cell_en_only
+                elif cur_cell:
+                    cell_en_only = cur_cell
+
+                field_groups[i_cell].append(cell_en_only)
+        fields = []
+        for field_group in field_groups.values():
+            field = "-".join([item for item in field_group if item.strip()])
+            for replace_text in [
+                "15 15 ",
+                "15-59 15-59 ",
+                "60-64 60-64 ",
+                "65 65 ",
+            ]:
+                field = field.replace(replace_text, "")
+            fields.append(field)
+        log.info(f"{fields=}")
+        return fields
+
+    def extract_data(self):
+        fields = self.get_fields()
+        return fields
+
     @classmethod
     def list_all(cls) -> list["OriginalDocTable"]:
         return [
-            OriginalDocTable(
-                "Basic-Population-Information"
-                + "-by-Districts-and-Divisional-Secretary-Divisions",
-                "A4. Migrant population"
-                + " by reason for migrating according to districts",
-                (110, 111),
-            ),
+            # OriginalDocTable(
+            #     "Basic-Population-Information"
+            #     + "-by-Districts-and-Divisional-Secretary-Divisions",
+            #     "A4. Migrant population"
+            #     + " by reason for migrating according to districts",
+            #     (110, 111),
+            # ),
             OriginalDocTable(
                 "Basic-Population-Information"
                 + "-by-Districts-and-Divisional-Secretary-Divisions",
                 "A5. Population by sex, age and district according to DSD",
-                (112, 121),
+                (112, 136),
+            ),
+            OriginalDocTable(
+                "Basic-Population-Information"
+                + "-by-Districts-and-Divisional-Secretary-Divisions",
+                "A6. Population by ethnicity and district according to DSD",
+                (137, 161),
+            ),
+            OriginalDocTable(
+                "Basic-Population-Information"
+                + "-by-Districts-and-Divisional-Secretary-Divisions",
+                "A7. Population by religion and district according to DSD",
+                (162, 185),
             ),
         ]
 
     @classmethod
     def extract_all(cls):
         for original_doc_table in cls.list_all():
-            original_doc_table.save_subset_pdf()
-            original_doc_table.extract_tables()
+            # original_doc_table.save_subset_pdf()
+            # original_doc_table.extract_tables()
+            original_doc_table.extract_data()
