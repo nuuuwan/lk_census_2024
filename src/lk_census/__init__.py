92cf97004aa5d799f834f01311b7c1f6bba70f99
nuuuwan <nuuuwan@gmail.com>
2025-11-12 18:24:47 +0530

    Remove OriginalDocReadMeMixin and associated README generation logic

--- src/lk_census/__init__.py
+++ src/lk_census/__init__.py
@@ -5,5 +5,5 @@ from lk_census.data_table import (DataTable, DataTableExtractDataCleanerMixin,
                                   DataTableExtractDataMixin,
                                   DataTableExtractDataValidateMixin,
                                   DataTableLoaderMixin, DataTablePDFMixin)
-from lk_census.original_doc import OriginalDoc, OriginalDocReadMeMixin
+from lk_census.original_doc import OriginalDoc
 from lk_census.readme import ReadMe
