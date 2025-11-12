6bc539078e11d8ed63cb007774916944131c6e8e
nuuuwan <nuuuwan@gmail.com>
2025-11-12 10:10:53 +0530

    add functionality to automatically stage and commit downloaded documents

--- workflows/download_original_docs.py
+++ workflows/download_original_docs.py
@@ -1,3 +1,4 @@
+import os
 
 from lk_census import OriginalDoc
 
@@ -5,6 +6,10 @@ from lk_census import OriginalDoc
 def main():
     OriginalDoc.download_all()
     OriginalDoc.build_readme()
+    os.system("git add original_docs")
+    os.system(
+        'git commit -m "[download_original_docs] $(date +%"Y-%m-%d %H:%M")"'
+    )
 
 
 if __name__ == "__main__":
