63c686b305171bde6aec1d206ce9edee15e5ca15
nuuuwan <nuuuwan@gmail.com>
2025-11-12 18:03:29 +0530

    Remove automatic git commit commands from download_original_docs.py

--- workflows/download_original_docs.py
+++ workflows/download_original_docs.py
@@ -1,15 +1,9 @@
-import os
-
 from lk_census import OriginalDoc
 
 
 def main():
     OriginalDoc.download_all()
     OriginalDoc.build_readme()
-    os.system("git add original_docs")
-    os.system(
-        'git commit -m "[download_original_docs] $(date +%"Y-%m-%d %H:%M")"'
-    )
 
 
 if __name__ == "__main__":
