adcea5191802dfdb52380497e276d65527e1e2b7
nuuuwan <nuuuwan@gmail.com>
2026-03-24 14:18:41 +0530

    Refactor metadata structure and update document handling
    - Removed the old Preliminary Report PDF and updated the metadata.json to use a new structured format with name, URL, and file path for each document.
    - Updated the OriginalDoc class to reflect changes in the metadata structure, including renaming attributes and modifying the download logic.
    - Added new PDF files for the Population, Housing, and CPH2024 Preliminary Reports.

--- src/lk_census/original_doc/OriginalDoc.py
+++ src/lk_census/original_doc/OriginalDoc.py
@@ -9,46 +9,38 @@ log = Log("OriginalDoc")
 
 @dataclass
 class OriginalDoc:
-    title: str
-    url_prefix: str
+    name: str
+    url: str
+    file_path: str
 
     DIR_ORIGINAL_DOCS = "original_docs"
-    URL_BASE = "https://www.statistics.gov.lk"
     METADATA_PATH = os.path.join(DIR_ORIGINAL_DOCS, "metadata.json")
 
     @property
-    def url(self) -> str:
-        return f"{self.URL_BASE}/{self.url_prefix}"
-
-    @property
     def doc_name(self) -> str:
-        safe_name = re.sub(r"\s+", " ", self.title)
-        safe_name = self.title.replace(" ", "-")
+        safe_name = re.sub(r"\s+", " ", self.name)
+        safe_name = self.name.replace(" ", "-")
         safe_name = "".join(
             char for char in safe_name if char.isalnum() or char == "-"
         )
         tokens = safe_name.split("-")
         return "-".join(tokens[:2])
 
-    @property
-    def pdf_path(self) -> str:
-        return os.path.join(self.DIR_ORIGINAL_DOCS, f"{self.doc_name}.pdf")
-
     def download(self) -> str:
-        if os.path.exists(self.pdf_path):
-            log.warning(f"{File(self.pdf_path)} exists. Skipping download.")
-            return self.pdf_path
+        if os.path.exists(self.file_path):
+            log.warning(f"{File(self.file_path)} exists. Skipping download.")
+            return self.file_path
         www = WWW(self.url)
         os.makedirs(self.DIR_ORIGINAL_DOCS, exist_ok=True)
-        www.download_binary(self.pdf_path)
-        log.info(f"Downloaded '{self.title}' to '{File(self.pdf_path)}'")
-        return self.pdf_path
+        www.download_binary(self.file_path)
+        log.info(f"Downloaded '{self.name}' to '{File(self.file_path)}'")
+        return self.file_path
 
     @classmethod
     def list_all(cls) -> list["OriginalDoc"]:
         doc_list = []
         for t in JSONFile(cls.METADATA_PATH).read():
-            doc = cls(*t)
+            doc = cls(name=t["name"], url=t["url"], file_path=t["file_path"])
             doc_list.append(doc)
         return doc_list
 
