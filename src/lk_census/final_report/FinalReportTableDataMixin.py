5e0ba931ecc30d56c2af1b9c50a339581d4b0705
nuuuwan <nuuuwan@gmail.com>
2026-07-12 11:43:31 +0530

    Add aggregation and expansion methods to FinalReportTableDataMixin for hierarchical data processing

--- src/lk_census/final_report/FinalReportTableDataMixin.py
+++ src/lk_census/final_report/FinalReportTableDataMixin.py
@@ -57,6 +57,53 @@ class FinalReportTableDataMixin:
                 d[key] = value
         return d
 
+    @staticmethod
+    def _aggregate(parent_id, d_list_for_parent):
+        parent = Ent.from_id(parent_id)
+        d_parent = dict(
+            region_id=parent_id,
+            region_name=parent.name,
+            region_ent_type=EntType.from_id(parent_id).name,
+        )
+        idx = {}
+        for d in d_list_for_parent:
+            for k, v in d.items():
+                if k.startswith("region_"):
+                    continue
+                if k not in idx:
+                    idx[k] = 0
+                idx[k] += v
+        for k, v in idx.items():
+            d_parent[k] = v
+        return d_parent
+
+    @staticmethod
+    def _expand_to_parent_types(d_list):
+        # Map
+        parent_id_to_d_list = {}
+        for ent_type in [EntType.COUNTRY, EntType.PROVINCE, EntType.ED]:
+            parent_id_key = ent_type.name.lower() + "_id"
+            for d in d_list:
+                child_region_id = d["region_id"]
+                child_region = Ent.from_id(child_region_id)
+                if ent_type == EntType.COUNTRY:
+                    parent_id = "LK"
+                else:
+                    parent_id = child_region.d[parent_id_key]
+                if parent_id not in parent_id_to_d_list:
+                    parent_id_to_d_list[parent_id] = []
+                parent_id_to_d_list[parent_id].append(d)
+
+        # Reduce
+        for parent_id, d_list_for_parent in parent_id_to_d_list.items():
+            d = FinalReportTableDataMixin._aggregate(
+                parent_id, d_list_for_parent
+            )
+            d_list.append(d)
+
+        d_list.sort(key=lambda x: x["region_id"])
+        return d_list
+
     def build_data(self):
         raw_data_list = self.raw_data_list
         if not raw_data_list:
@@ -71,5 +118,8 @@ class FinalReportTableDataMixin:
             d = self._build_data_item(raw_data, n_fields, fields)
             if d:
                 d_list.append(d)
+
+        d_list = self._expand_to_parent_types(d_list)
+
         self.data_file.write(d_list)
         log.info(f"Wrote {len(d_list)} rows to {self.data_file}")
