76eededf7f0b6a31e393280daa8d5f919dbd82e5
nuuuwan <nuuuwan@gmail.com>
2026-07-13 12:03:23 +0530

    Refactor add_fields.py to streamline table processing and remove unused imports

--- workflows/add_fields.py
+++ workflows/add_fields.py
@@ -1,21 +1,18 @@
 import os
-import sys
 
 from lk_census.final_report.table import FinalReportTable
-from lk_census.readme.ReadMe import ReadMe
 
 if __name__ == "__main__":
-    table_num = sys.argv[1]
-    table = FinalReportTable.from_table_num(table_num)
+    table_list = FinalReportTable.list()
 
-    os.system(f"code {table.original_pdf_file.path}")
+    for table in table_list:
+        if table.build_status != 2:
+            continue
 
-    if not table.fields_file.exists:
-        table.fields_file.write(
-            dict(original_key="", other_keys=[], is_summable=False)
-        )
-        os.system(f"code {table.fields_file.path}")
-    else:
-        table.build()
-        ReadMe().build()
-        os.system(f"code {table.data_file.path}")
+        os.system(f"code {table.original_pdf_file.path}")
+
+        if not table.fields_file.exists:
+            table.fields_file.write(
+                dict(original_key="", other_keys=[], is_summable=False)
+            )
+            os.system(f"code {table.fields_file.path}")
