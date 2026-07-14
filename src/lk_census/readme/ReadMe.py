22a971bedba2e901daf54db975c14ab337fa6992
nuuuwan <nuuuwan@gmail.com>
2026-07-14 15:27:21 +0530

    Refactor ReadMe and ReadMeFinalReportMixin classes
    - Simplified the `get_lines_for_final_report` method in `ReadMe` by removing unnecessary parameters and streamlining the logic for generating report lines.
    - Removed redundant methods and code in `ReadMeFinalReportMixin`, consolidating functionality for generating final report tables and improving readability.
    - Introduced a static method `_group_by_chapter` to organize final report tables by chapter, enhancing the structure of the final report output.
    - Updated the final report status display to include percentage representation of datasets.

--- src/lk_census/readme/ReadMe.py
+++ src/lk_census/readme/ReadMe.py
@@ -50,13 +50,9 @@ class ReadMe(ReadMeDataTableMixin, ReadMeFinalReportMixin):
         final_report_table_list = FinalReportTable.list()
         n_datasets_non_final_table = len(data_table_list)
         return (
-            self.get_lines_for_header(
-                data_table_list, final_report_table_list
-            )
+            self.get_lines_for_header(data_table_list, final_report_table_list)
             + self.get_lines_for_xlsx_data_tables(data_table_list)
-            + self.get_lines_for_final_report(
-                n_datasets_non_final_table, final_report_table_list
-            )
+            + self.get_lines_for_final_report(final_report_table_list)
             + self.get_lines_for_footer()
         )
 
--- src/lk_census/readme/ReadMeFinalReportMixin.py
+++ src/lk_census/readme/ReadMeFinalReportMixin.py
@@ -5,211 +5,6 @@ from utils_future import Markdown
 
 
 class ReadMeFinalReportMixin:
-    URL_FINAL_REPORT = FinalReportTable.URL_FINAL_REPORT
-
-    def flatten_dict(self, d):
-        flat_d = {}
-        for k, v in d.items():
-            if k == "values":
-                for k1, v1 in v.items():
-                    flat_d[k1.replace("_", " ").title()] = v1
-            else:
-                flat_d[k.replace("_", " ").title()] = v
-        return flat_d
-
-    def get_lines_for_final_report_table_with_pdf_data(
-        self, final_report_table
-    ):
-        assert final_report_table.original_pdf_image_file.exists
-        lines = [
-            "### Original Table",
-            "",
-            f"![{final_report_table.table_id}]"
-            + f"({final_report_table.original_pdf_image_file.path})",
-            "",
-        ]
-        return lines
-
-    def get_lines_for_final_report_table_with_raw_data(
-        self, final_report_table
-    ):
-        assert final_report_table.raw_data_file.exists
-        lines = []
-
-        example_rows = final_report_table.raw_data_list[:10]
-        max_cols = max(len(row) for row in example_rows)
-
-        d_list = [
-            {
-                f"Col {i}": row[i] if i < len(row) else "--"
-                for i in list(range(max_cols))
-            }
-            for row in example_rows
-        ]
-        lines.extend(
-            [
-                "### Raw Data (first 10 rows)",
-                "",
-            ]
-            + Markdown.table(d_list)
-        )
-
-        return lines
-
-    def get_lines_for_final_report_table_with_raw_data_complicated(
-        self, final_report_table
-    ):
-        assert final_report_table.raw_data_file.exists
-        lines = []
-
-        example_rows = final_report_table.raw_data_list[:10]
-        max_cols = max(len(row) for row in example_rows)
-
-        d_list = [
-            {
-                f"Col {i}": row[i] if i < len(row) else "--"
-                for i in list(range(max_cols))
-            }
-            for row in example_rows
-        ]
-        lines.extend(
-            [
-                "### Raw Data (first 10 rows)",
-                "",
-            ]
-            + Markdown.table(d_list)
-        )
-
-        return lines
-
-    def get_lines_for_final_report_table_with_structured_data(
-        self, final_report_table
-    ):
-        assert final_report_table.data_file.exists
-        N_DISPLAY = 10
-
-        d_list_for_table = [
-            self.flatten_dict(d) for d in final_report_table.data_list
-        ]
-        n_total = len(d_list_for_table)
-
-        title = "Data Table"
-        if N_DISPLAY < n_total:
-            title += f" (first {N_DISPLAY} of {n_total} rows)"
-            d_list_for_table = d_list_for_table[:N_DISPLAY]
-
-        lines = []
-        lines.extend(
-            [
-                f"### {title}",
-                "",
-            ]
-            + Markdown.table(d_list_for_table)
-        )
-        lines.extend(
-            [
-                "### Example Data Row (JSON)",
-                "",
-                "```json",
-                json.dumps(d_list_for_table[0], indent=4),
-                "```",
-                "",
-            ]
-        )
-
-        return lines
-
-    def get_lines_for_final_report_table_with_lanka_data(
-        self, final_report_table
-    ):
-        assert final_report_table.data_file.exists
-        N_DISPLAY_LINES = 30
-
-        data_lines = json.dumps(
-            final_report_table.lanka_data, indent=4
-        ).split("\n")
-        n_data_lines = len(data_lines)
-
-        if n_data_lines > N_DISPLAY_LINES:
-            data_lines = data_lines[:N_DISPLAY_LINES] + ["..."]
-            title = f"Lanka Data (first {N_DISPLAY_LINES} of {n_data_lines} lines)"
-        else:
-            title = "Lanka Data"
-
-        lines = []
-        lines.extend(
-            [
-                f"### {title}",
-                "",
-                "```json",
-            ]
-            + data_lines
-            + [
-                "```",
-                "",
-            ]
-        )
-
-        return lines
-
-    def _get_lines_for_final_report_table_inner(self, final_report_table):
-        status = final_report_table.build_status
-
-        if status == 5:
-            return self.get_lines_for_final_report_table_with_lanka_data(
-                final_report_table
-            )
-
-        if status == 4 or status == 5:
-            return self.get_lines_for_final_report_table_with_structured_data(
-                final_report_table
-            )
-
-        if status == 3:
-            return self.get_lines_for_final_report_table_with_raw_data(
-                final_report_table
-            )
-
-        if status == 2:
-            return self.get_lines_for_final_report_table_with_raw_data_complicated(
-                final_report_table
-            )
-
-        if status == 1:
-            return self.get_lines_for_final_report_table_with_pdf_data(
-                final_report_table
-            )
-
-        raise ValueError(
-            f"Final report table {final_report_table.table_num} "
-            + "does not have any data file"
-        )
-
-    def get_lines_for_final_report_table(self, i_dataset, final_report_table):
-        description = FinalReportTable.STATUS_LABELS[
-            final_report_table.build_status
-        ]
-        lines = [
-            f"## {i_dataset}. [{final_report_table.table_name}]"
-            + f"({final_report_table.dir_data})",
-            "",
-            f"*Build Status (**{final_report_table.build_status}**/5)"
-            + f" {description}*",
-            "",
-        ]
-        lines.extend(
-            self._get_lines_for_final_report_table_inner(final_report_table)
-        )
-        lines.extend(
-            [
-                "### Source",
-                "",
-                f"- <{final_report_table.source_url}>"
-                + f" (Table {final_report_table.table_num})",
-                "",
-            ]
-        )
-        return lines
 
     def get_lines_for_final_report_status(self, final_report_table_list):
         status_to_n = {}
@@ -220,36 +15,60 @@ class ReadMeFinalReportMixin:
             status_to_n[status] += 1
 
         lines = [
-            "## `Final Report Build Status`",
+            "### Final Report Build Status",
             "",
         ]
         status_to_n = dict(sorted(status_to_n.items(), key=lambda x: x[0]))
+        total = len(final_report_table_list)
         d_list = [
             dict(
-                status=status,
+                status=f"**{status}**/5",
                 status_label=FinalReportTable.STATUS_LABELS[status],
-                n=n,
+                n=f"**{n:,}**",
+                p=f"**{n / total:.1%}**",
             )
             for status, n in status_to_n.items()
         ]
         lines.extend(Markdown.table(d_list))
         return lines
 
-    def get_lines_for_final_report(
-        self, n_datasets_non_final_table, final_report_table_list
-    ):
-        len(final_report_table_list)
+    def get_lines_for_final_report_table(self, final_report_table):
+        return [
+            f"- Table {final_report_table.table_num}"
+            + f" - [{final_report_table.table_name}]"
+            + f"({final_report_table.dir_data})"
+        ]
 
-        lines = self.get_lines_for_final_report_status(
-            final_report_table_list
-        )
-        i_dataset = n_datasets_non_final_table + 1
-        for final_report_table in final_report_table_list:
+    @staticmethod
+    def _group_by_chapter(table_list):
+        idx = {}
+        for table in table_list:
+            chapter = table.chapter_num
+            if chapter not in idx:
+                idx[chapter] = []
+            idx[chapter].append(table)
+        return idx
 
-            lines.extend(
-                self.get_lines_for_final_report_table(
-                    i_dataset, final_report_table
+    def get_lines_for_final_report(self, final_report_table_list):
+        n = len(final_report_table_list)
+
+        lines = [
+            f"## Datasets from Final Report (**{n:,}**)",
+            "",
+        ]
+
+        idx = self._group_by_chapter(final_report_table_list)
+        for chapter, table_list_for_chapter in idx.items():
+            lines.extend([f"### Chapter {chapter}", ""])
+            for final_report_table in table_list_for_chapter:
+                lines.extend(
+                    self.get_lines_for_final_report_table(final_report_table)
                 )
-            )
-            i_dataset += 1
+            lines.append("")
+        lines.append("")
+        lines.extend(
+            [""]
+            + self.get_lines_for_final_report_status(final_report_table_list)
+        )
+
         return lines
