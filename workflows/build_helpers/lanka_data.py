005ebcd3b2838dfca5fac575abcc1cbb57cc8248
nuuuwan <nuuuwan@gmail.com>
2026-07-14 11:05:46 +0530

    Add new scripts for handling data processing and metadata management in the build_helpers

---
+++ workflows/build_helpers/lanka_data.py
@@ -0,0 +1,50 @@
+import json
+import os
+
+from lk_census.final_report.table import FinalReportTable
+from lk_census.readme.ReadMe import ReadMe
+
+
+def print_line():
+    print("-" * 40)
+
+
+if __name__ == "__main__":
+
+    table_list = FinalReportTable.list()
+
+    for table in table_list:
+        if table.build_status != 4:
+            continue
+
+        print_line()
+
+        print(table.table_num)
+
+        print(
+            table.build_status,
+            FinalReportTable.STATUS_LABELS[table.build_status],
+        )
+        print_line()
+
+        print(json.dumps(table.data_list[0], indent=4))
+        print_line()
+
+        if not table.is_lanka_data_metadata_complete:
+            lanka_data_metadata = {}
+            if table.lanka_data_metadata_file.exists:
+                lanka_data_metadata = table.lanka_data_metadata_file.read()
+            lanka_data_metadata |= {
+                "what_label": f'"{table.table_name}"',
+            }
+            table.lanka_data_metadata_file.write(lanka_data_metadata)
+            table.lanka_data_metadata_file.open("code")
+
+        else:
+            lanka_data = table.build_lanka_data()
+            print(json.dumps(lanka_data, indent=4))
+            print_line()
+            if lanka_data is not None:
+                table.lanka_data_file.open("code")
+
+        break
