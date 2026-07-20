e4f1734a48e716a350d345beb316d0014b2b7bcc
nuuuwan <nuuuwan@gmail.com>
2026-07-20 19:33:38 +0530

    Refactor pipeline.py to move FinalReport.build() into not_running function

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,6 +1,10 @@
 from lk_census import FinalReport, ReadMe, XLSXDataTable
 
+
+def not_running():
+    FinalReport.build()
+
+
 if __name__ == "__main__":
     XLSXDataTable.build_all()
-    FinalReport.build()
     ReadMe().build()
