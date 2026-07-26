d72639125dedc120b796f49b8e5fbca0dce050f4
nuuuwan <nuuuwan@gmail.com>
2026-07-26 14:29:53 +0530

    Refactor import statements in FinalReportTable for improved readability

--- src/lk_census/final_report/table/FinalReportTable.py
+++ src/lk_census/final_report/table/FinalReportTable.py
@@ -1,30 +1,22 @@
-import os
 
 from utils_future import Log
 
 from lk_census.common.CommonTableReadMeMixin import CommonTableReadMeMixin
 from lk_census.final_report.table.data import FinalReportTableDataMixin
-from lk_census.final_report.table.FinalReportTableBase import (
-    FinalReportTableBase,
-)
-from lk_census.final_report.table.FinalReportTableBuildMixin import (
-    FinalReportTableBuildMixin,
-)
-from lk_census.final_report.table.FinalReportTableIsComplicatedMixin import (
-    FinalReportTableIsComplicatedMixin,
-)
-from lk_census.final_report.table.FinalReportTableLoadMixin import (
-    FinalReportTableLoadMixin,
-)
-from lk_census.final_report.table.FinalReportTablePDFMixin import (
-    FinalReportTablePDFMixin,
-)
-from lk_census.final_report.table.FinalReportTableRawDataMixin import (
-    FinalReportTableRawDataMixin,
-)
-from lk_census.final_report.table.lanka_data.FinalReportLankaDataMixin import (
-    FinalReportLankaDataMixin,
-)
+from lk_census.final_report.table.FinalReportTableBase import \
+    FinalReportTableBase
+from lk_census.final_report.table.FinalReportTableBuildMixin import \
+    FinalReportTableBuildMixin
+from lk_census.final_report.table.FinalReportTableIsComplicatedMixin import \
+    FinalReportTableIsComplicatedMixin
+from lk_census.final_report.table.FinalReportTableLoadMixin import \
+    FinalReportTableLoadMixin
+from lk_census.final_report.table.FinalReportTablePDFMixin import \
+    FinalReportTablePDFMixin
+from lk_census.final_report.table.FinalReportTableRawDataMixin import \
+    FinalReportTableRawDataMixin
+from lk_census.final_report.table.lanka_data.FinalReportLankaDataMixin import \
+    FinalReportLankaDataMixin
 
 log = Log("FinalReportTable")
 
