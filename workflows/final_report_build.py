3bb04ca68e00aa3cf7b29cc3ca1bfef59465e257
nuuuwan <nuuuwan@gmail.com>
2026-07-26 13:25:56 +0530

    Update README with correct last updated date, fix JSON formatting, and refactor final report build process for improved clarity

--- workflows/final_report_build.py
+++ workflows/final_report_build.py
@@ -1,6 +1,6 @@
 from lk_census import FinalReport, ReadMe
 
 if __name__ == "__main__":
-    FinalReport.clean()
-    FinalReport.build()
+    FinalReport.clean_all()
+    FinalReport.build_all()
     ReadMe().build()
